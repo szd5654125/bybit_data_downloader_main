5cfd315adac9e70ee35879c96d77e266180c6517
Kailun Qiu <113058390+szd5654125@users.noreply.github.com>
2025-03-03 23:52:46 +0800

    没做什么修改

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ headers = {
     "sec-fetch-dest": "empty",
     "sec-fetch-mode": "cors",
     "sec-fetch-site": "same-site",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 "
+                  "Safari/537.36"
 }
 
 for symbol in symbols:
